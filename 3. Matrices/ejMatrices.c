08c9c828e3e4cb24f212b62cd3e3f57abc2f430c
María Martínez <mariadanielamartinezr@gmail.com>
2025-07-02 00:32:27 -0400

    Update ejMatrices.c

--- 3. Matrices/ejMatrices.c
+++ 3. Matrices/ejMatrices.c
@@ -15,19 +15,6 @@ int main()
         }
     }
 
-    //Mostrar matriz original
-
-    for (i=0;i<4;i++){
-        for (j=0;j<4;j++){
-            printf("%d\t",matriz[i][j]);
-        }
-        printf("\n");
-    }
-
-    //Ordenar la matriz de forma decreciente
-
-
-
     //Sumar la diagonal principal y secundaria de la matriz
     int sumaPrincipal =0, sumaSecundaria=0;
 
@@ -36,7 +23,7 @@ int main()
             if (i==j){
                 sumaPrincipal+=matriz[i][j];
             }
-            if (i+j==3){
+            if ((i+j)==3){
                 sumaSecundaria+=matriz[i][j];
             }
         }
@@ -56,18 +43,63 @@ int main()
         }
     }
 
-    //Mostrar la matriz traspuesta
-    printf("\nMatriz traspuesta:\n");
-    for (i=0;i<4;i++){
-        for (j=0;j<4;j++){
-            printf("%d\t",matriz[j][i]);
+    //Mostrar matriz original
+
+    printf("---------------- Matriz original ---------------\n");
+    for ( i = 0; i < 4; i++) {
+        for ( j = 0; j < 4; j++) {
+            printf("%d  ", matriz[i][j]);
         }
         printf("\n");
     }
-    printf("\nSuma principal: %d",sumaPrincipal);
-    printf("\nSuma secundaria: %d",sumaSecundaria);
-    printf("\nCantidad de numeros pares: %d",cantidadPares);
-    printf("\nCantidad de numeros impares: %d",cantidadImpares);
+
+
+
+    //Matriz ordenada de forma decreciente
+    int filas = 4, columnas=4;
+    int totalElementos=filas*columnas;
+    for (i = 0; i < totalElementos - 1; i++) {
+        for ( j = 0; j < totalElementos - i - 1; j++) {
+            //Calculo de indices para el elemento j
+            int fila1 = j /columnas;
+            int columna1 = j % columnas;
+
+            //Calcular indices para el siguiente elemento
+            int fila2 = (j + 1) / columnas;
+            int columna2 = (j +1) % columnas;
+
+            if (matriz[fila1][columna1] < matriz[fila2][columna2]){
+                int auxiliar = matriz [fila1][columna1];
+                matriz [fila1][columna1]=matriz[fila2][columna2];
+                matriz [fila2][columna2]=auxiliar;
+            }
+        }
+    }
+
+    printf("\n----- Matriz ordenada de forma decreciente -----\n");
+    for (i=0;i<4;i++){
+        for (j=0;j<4;j++){
+            printf("%d  ",matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    //Mostrar la matriz traspuesta
+    printf("\n--------------- Matriz traspuesta --------------\n");
+    for (i=0;i<4;i++){
+        for (j=0;j<4;j++){
+            printf("%d  ",matriz[j][i]);
+        }
+        printf("\n");
+    }
+
+    printf("\n------------- Suma de las columnas -------------\n");
+    printf("\nSuma principal: %d\n",sumaPrincipal);
+    printf("Suma secundaria: %d\n",sumaSecundaria);
+
+    printf("\n------------- Cantidad de numeros --------------\n");
+    printf("\nPares: %d\n",cantidadPares);
+    printf("Impares: %d\n",cantidadImpares);
 
     return 0;
 }
