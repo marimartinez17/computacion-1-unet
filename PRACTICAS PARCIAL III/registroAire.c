fa5ef426c00e593884287c69ad3ade2ce99ac22e
María Martínez <mariadanielamartinezr@gmail.com>
2025-08-06 02:03:01 -0400

    Update and rename registroAire to registroAire.c

--- PRACTICAS PARCIAL III/registroAire.c
+++ PRACTICAS PARCIAL III/registroAire.c
@@ -3,9 +3,7 @@
 #include <time.h>
 #include <locale.h>
 
-#define MAX 10000
-
-//Definicion de constantes
+// Definicion de struct para el registro realizado por el sensor
 
 struct RegistroAire{
     int id;
@@ -16,6 +14,7 @@ struct RegistroAire{
     char fecha[12];
 };
 
+// Funcion para simular valores de medicion (no asigna ID) 
 void medicionAire(struct RegistroAire *sensor){
 
     sensor->pm25 = ((float)rand() / (RAND_MAX)) * (100); //Del 0 al 100
@@ -25,11 +24,12 @@ void medicionAire(struct RegistroAire *sensor){
     int dia, mes, year;
     dia = rand () % 28 + 1;
     mes = rand () % 12 + 1;
-    year = rand () % 36 + 1990;
+    year = rand () % 36 + 1990; //Entre 1990 y 2025
     sprintf(sensor->fecha, "%02d/%02d/%d",dia,mes,year);
 
 }
 
+// Funcion para verificar que el ID aleatorio sea unico
 int chequearID(struct RegistroAire *sensor, int i, int nuevoID){
    for (int i=0; i<4;i++){
         if (sensor[i].id == nuevoID){
@@ -39,6 +39,7 @@ int chequearID(struct RegistroAire *sensor, int i, int nuevoID){
     return 0;
 }
 
+// Funcion para determinar el mayor nivel de PM25
 float mayorPM25(struct RegistroAire *sensor, int tamano){
     float mayor = 0.0;
     for (int i=0;i<tamano;i++){
@@ -49,6 +50,7 @@ float mayorPM25(struct RegistroAire *sensor, int tamano){
     return mayor;
 }
 
+// Funcion para calcular el promedio de CO
 float promedioCO(struct RegistroAire *sensor, int tamano){
     float acumCO = 0.0;
     for (int i=0;i<tamano;i++){
@@ -57,6 +59,7 @@ float promedioCO(struct RegistroAire *sensor, int tamano){
     return acumCO / tamano;
 }
 
+// Funcion para determinar la temperatura minima
 float tempMinima (struct RegistroAire *sensor, int tamano){
     float temperaturaMinima = 999999.9;
 
@@ -68,6 +71,7 @@ float tempMinima (struct RegistroAire *sensor, int tamano){
     return temperaturaMinima;
 }
 
+// Funcion para consultar por ID
 void buscarID(struct RegistroAire *Registros, int tamano, int horas[]){
     int buscador, bandera = 0;
     printf("Ingrese el ID a consultar: ");
@@ -91,6 +95,7 @@ void buscarID(struct RegistroAire *Registros, int tamano, int horas[]){
     }
 }
 
+// Funcion para consultar por fecha
 void buscarFecha(struct RegistroAire *Registros, int tamano, int horas[]){
     char buscador[11], bandera = 0;
     int dia,mes,year;
@@ -125,14 +130,18 @@ void buscarFecha(struct RegistroAire *Registros, int tamano, int horas[]){
     }
 }
 
+// Funcion para consultar un registro
 void buscarRegistros(struct RegistroAire *Registros, int tamano, int horas[]){
     int respuesta;
+    
+    // Desplegar opciones
     printf("\n\nMETODOS DE BUSQUEDA: \n1. ID\n2. FECHA");
     printf("\n\nIngrese la opcion de busqueda: ");
     scanf(" %d", &respuesta);
 
     system("CLS");
 
+    // Iterar opciones
     switch(respuesta){
     case 1:
         buscarID(Registros,tamano,horas);
@@ -149,13 +158,18 @@ int main()
 {
     srand(time(NULL));
 
-    int size = 4, horas[4] = {0,6,12,18};
+    int size = 4, horas[4] = {0,6,12,18}; //Horas en las que se realizaran los registros
+
+    //Declaracion del array dinamico
     struct RegistroAire* Registros = (struct RegistroAire*)malloc(size * sizeof(struct RegistroAire));
+    
+    // Verificar que se haya asignado memoria
     if (Registros == NULL){
         printf("Error: Memoria no asignada.\n");
         return 1;
     }
 
+    // Generar ID's aleatorios unicos
     for (int i=0;i<size;i++){
         int nuevoID;
         do {
@@ -163,12 +177,14 @@ int main()
         } while (chequearID(&Registros[i],i,nuevoID)==1);
 
         Registros[i].id = nuevoID;
+
+        // Simular los demas datos
         medicionAire(&Registros[i]);
     }
 
      // Mostrar los registros
 
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++) { 
         printf("| Registro #%d | HORA: %02d:00|===============|\n", i + 1, horas[i]);
         printf("ID: %d\n", Registros[i].id);
         printf("Fecha: %s\n", Registros[i].fecha);
@@ -195,6 +211,8 @@ int main()
 
     char res;
 
+    // Dar la opcion de consultar registros
+    
     printf("DESEA CONSULTAR UN REGISTRO? (S/N): ");
     scanf(" %c", &res);
 
@@ -203,6 +221,7 @@ int main()
         buscarRegistros(Registros,size,horas);
     }
 
+    // Liberar espacios de memoria
     free(Registros);
 
     return 0;
